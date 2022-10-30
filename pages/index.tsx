dfbf85d677377f51cf7cb9fcc46c0c6b2983732c
Ron Almog <ron8132@gmail.com>
2022-10-30 19:13:51 -0400

    add a record, refresh page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,53 @@
-import Head from "next/head";
+import { GetServerSideProps } from "next";
 import Image from "next/image";
-import { format } from "path";
+import { useRouter } from "next/router";
 import { useState } from "react";
-import { setFlagsFromString } from "v8";
-import styles from "../styles/Home.module.css";
+import { prisma } from "../lib/prisma";
 
 interface FormData {
   title: string;
   content: string;
   id: string;
 }
-export default function Home() {
+
+interface Notes {
+  notes: {
+    id: string;
+    title: string;
+    content: string;
+  }[];
+}
+export default function Home({ notes }: Notes) {
+  const createNote = async (data: FormData) => {
+    try {
+      fetch("http://localhost:3000/api/createNote", {
+        body: JSON.stringify(data),
+        headers: {
+          "content-Type": "application/json",
+        },
+        method: "POST",
+      }).then(() => {
+        setForm({ title: "", content: "", id: "" });
+        refreshData();
+      });
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const router = useRouter();
+  const refreshData = () => {
+    router.replace(router.asPath); // refresh page
+  };
+
+  const handleSubmit = async (data: FormData) => {
+    try {
+      createNote(data);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
   const [form, setForm] = useState<FormData>({
     title: "",
     content: "",
@@ -22,6 +59,7 @@ export default function Home() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
+          handleSubmit(form);
         }}
         className="w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch"
       >
@@ -43,6 +81,35 @@ export default function Home() {
           Add +
         </button>
       </form>
+      <div className="w-auto min-w-[25%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch">
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id} className="border-b border-gray-600 p-2">
+              <div className="flex justify-between">
+                <div className="flex-1">
+                  <h3 className="font-bold">{note.title}</h3>
+                  <p className="text-sm">{note.content}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const notes = await prisma.note.findMany(); // basic version
+  const notes = await prisma.note.findMany({
+    select: {
+      title: true,
+      content: true,
+      id: true,
+    },
+  });
+
+  return {
+    props: { notes },
+  };
+};
